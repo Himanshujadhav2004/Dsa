79db20b5060313b832400eaeec06f34b1aa3dab0
Himanshujadhav2004 <himanshujadhav341@gmail.com>
2025-10-28 20:41:08 +0530

    Two Sum optimal

--- TwoSum.java
+++ TwoSum.java
@@ -1,7 +1,10 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        // Brute-force approach
         int n = nums.length;
 
+       
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (nums[i] + nums[j] == target) {
@@ -11,5 +14,23 @@ class Solution {
         }
 
         return new int[]{}; 
+
+
+        // Optimal approach using HashMap
+         HashMap<Integer,Integer> map =  new HashMap<>();
+
+        for(int i =0:i,nums.length;i++){
+            int nedded = traget - nums[i];
+            if(map.containsKey(nedded)){
+                return new int[]{map.get(nedded),i};
+            }
+            else{
+                map.put(nums[i],i);
+            }
+            return new int[]{};
+        }
+
     }
+
+
 }
