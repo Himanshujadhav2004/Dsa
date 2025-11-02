a200a02de7b114766ad9132f808b51544a5203d7
Himanshujadhav2004 <himanshujadhav341@gmail.com>
2025-11-02 12:43:01 +0530

    maximumSubarray optimal approch

--- Array/MaximumSubarray.java
+++ Array/MaximumSubarray.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxSubArray(int[] nums) {
 
-        //brut
+        //brut time complexity O(n^3) space O(1)
         int n = nums.length;
         int maximum=Integer.MIN_VALUE;
         int sum=0;
@@ -15,7 +15,7 @@ maximum=Math.max(sum,maximum);
             }
         }
 
-        //better 
+        //better  time complexity O(n^2) space O(1)
         for(int i=0;i<n;i++){
             sum=0;
             for(int j=i;j<n;j++){
@@ -25,4 +25,17 @@ maximum=Math.max(sum,maximum);
         }
         return maximum;
     }
+
+    //optimal time complexity O(n) space O(1)
+            for(int i=0;i<n;i++){
+        sum += nums[i];
+        if(sum>maximum){
+            maximum=sum;
+        }
+            if(sum<0){
+                  sum=0;
+            }
+        }
+        return maximum;
+    
 }
